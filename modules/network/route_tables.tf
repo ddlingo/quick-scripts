5d412768e52883521e9d431b6f00fdc80495eb2c
Deadrick Lingo <ddlingo@gmail.com>
2025-07-23 10:38:06 -0400

    Create route_tables.tf

---
+++ modules/network/route_tables.tf
@@ -0,0 +1,41 @@
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name        = "${var.context.name_prefix}-public-rt"
+    owner       = var.context.owner
+    environment = var.context.environment
+    managedby   = var.context.managed_by
+  }
+}
+
+resource "aws_route_table" "private_rt" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat_gw.id
+  }
+
+  tags = {
+    Name        = "${var.context.name_prefix}-private-rt"
+    owner       = var.context.owner
+    environment = var.context.environment
+    managedby   = var.context.managed_by
+  }
+}
+
+resource "aws_route_table_association" "public_assoc" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+resource "aws_route_table_association" "private_assoc" {
+  subnet_id      = aws_subnet.private.id
+  route_table_id = aws_route_table.private_rt.id
+}
