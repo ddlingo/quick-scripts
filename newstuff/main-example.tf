e7a33600f1aa53dd2a3aeec4053c0298f95b8b07
Deadrick Lingo <ddlingo@gmail.com>
2025-07-24 12:58:32 -0400

    Create main-example.tf

---
+++ newstuff/main-example.tf
@@ -0,0 +1,8 @@
+module "network" {
+  source           = "../modules/network"
+  network_name     = var.network_name
+  subnet_name      = var.subnet_name
+  cidr             = var.cidr
+  gateway_ip       = var.gateway_ip
+  dns_nameservers  = var.dns_nameservers
+}
