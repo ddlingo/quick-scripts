27e1927d36de1d7b903309aca31a8ecc2467326a
Deadrick Lingo <ddlingo@gmail.com>
2025-07-28 11:11:05 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,9 +1,11 @@
 variable "account_id" {
-  type = string
+  type        = string
+  description = "Account ID for the environment"
 }
 
 variable "build_user" {
-  type = string
+  type        = string
+  description = "User or system initiating the build"
 }
 
 variable "business" {
@@ -12,20 +14,24 @@ variable "business" {
     formatted = string
     friendly  = string
   })
+  description = "Business context object"
 }
 
 variable "customer" {
   type = object({
     name = string
   })
+  description = "Customer information object"
 }
 
 variable "include_customer_label" {
-  type = bool
+  type        = bool
+  description = "Include the customer label in resource tags/labels"
 }
 
 variable "environment" {
-  type = string
+  type        = string
+  description = "Deployment environment name"
 }
 
 variable "organization" {
@@ -34,26 +40,32 @@ variable "organization" {
     formatted = string
     friendly  = string
   })
+  description = "Organization context object"
 }
 
 variable "label_order" {
-  type = list(string)
+  type        = list(string)
+  description = "Order in which labels are applied"
 }
 
 variable "owner" {
-  type = string
+  type        = string
+  description = "Owner of the deployment"
 }
 
 variable "partition" {
-  type = string
+  type        = string
+  description = "Partition (e.g., 'aws', 'openstack')"
 }
 
 variable "region" {
-  type = string
+  type        = string
+  description = "Region for resource deployment"
 }
 
 variable "root_module" {
-  type = string
+  type        = string
+  description = "Name of the root module"
 }
 
 variable "security_boundary" {
@@ -62,4 +74,32 @@ variable "security_boundary" {
     formatted = string
     friendly  = string
   })
+  description = "Security boundary context"
+}
+
+# Existing networking variables
+
+variable "network_name" {
+  type        = string
+  description = "Name of the OpenStack network"
+}
+
+variable "subnet_name" {
+  type        = string
+  description = "Name of the OpenStack subnet"
+}
+
+variable "cidr" {
+  type        = string
+  description = "CIDR block for the subnet"
+}
+
+variable "gateway_ip" {
+  type        = string
+  description = "Gateway IP for the subnet"
+}
+
+variable "dns_nameservers" {
+  type        = list(string)
+  description = "List of DNS nameservers"
 }
