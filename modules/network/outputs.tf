60f9ad736a0e2e729f005794524af70f270f2116
Deadrick Lingo <ddlingo@gmail.com>
2025-07-28 11:01:16 -0400

    Update outputs.tf

--- modules/network/outputs.tf
+++ modules/network/outputs.tf
@@ -1,11 +1,7 @@
-output "vpc_id" {
-  value = aws_vpc.main.id
+output "network_id" {
+  value = openstack_networking_network_v2.network.id
 }
 
-output "public_subnet_id" {
-  value = aws_subnet.public.id
-}
-
-output "private_subnet_id" {
-  value = aws_subnet.private.id
+output "subnet_id" {
+  value = openstack_networking_subnet_v2.subnet.id
 }
