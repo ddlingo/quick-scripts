9faa9dbccfa6d46cd4335c885bf583dbe2d1a72a
Deadrick Lingo <ddlingo@gmail.com>
2025-09-12 10:48:02 -0400

    Add variables for IAM configuration in Terraform

---
+++ iam-sci/variables.tf
@@ -0,0 +1,76 @@
+variable "project_name" {}
+variable "domain_id" { default = "default" }
+variable "federated_group_local_name" {}
+variable "openstack_role_name" {}
+variable "bootstrap_admin_name" { default = "" }
+variable "bootstrap_admin_password" { default = "" }
+
+# Example tags/context variables
+variable "environment" {}
+variable "owner" {}
+variable "organization" {
+  type = object({
+    formatted = string
+    friendly  = string
+  })
+}
+variable "security_boundary" {
+  type = object({
+    formatted = string
+    friendly  = string
+  })
+}
+variable "business" {
+  type = object({
+    formatted = string
+    friendly  = string
+  })
+}
+variable "cloud_provider" {
+  type = object({
+    name      = string
+    formatted = string
+    friendly  = string
+  })
+}
+variable "cloud_partition" {
+  type = object({
+    name      = string
+    formatted = string
+    friendly  = string
+  })
+}
+variable "minor_security_boundary" {
+  type = object({
+    name      = string
+    formatted = string
+    friendly  = string
+  })
+}
+variable "business_subsection" {
+  type = object({
+    name      = string
+    formatted = string
+    friendly  = string
+  })
+}
+variable "account_identifier" {
+  type = object({
+    name      = string
+    formatted = string
+    friendly  = string
+  })
+}
+variable "customer" {
+  type = object({
+    formatted = string
+    friendly  = string
+  })
+}
+variable "parent_domain_internal" {}
+variable "parent_domain_external" {}
+
+# Example compute/network variables
+variable "image_name" {}
+variable "flavor_name" {}
+variable "keypair_name" {}
